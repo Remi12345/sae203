0fc4dfd2fea9903f3733760b810795d3d31ca637
Remi12345 <remi.droit@etudiant.univ-reims.fr>
2022-03-04 16:08:04 +0100

    imooration

--- form_recherche.php
+++ form_recherche.php
@@ -5,20 +5,20 @@ require 'header.php';
 
 ?>
 
-<form action="" method="">
+    <form action="" method="">
             <label for="jnba_nom">Retrouvez vos joueurs préférés : </label>
             <input type="search" autocomplete="off" list="meilleurs_joueurs_nba" id="jnba_nom" name="joueurs"/>
             <datalist id="meilleurs_joueurs_nba">
-            <option value="Lebron James">
-            <option value="Michael Jordan">
-            <option value="Wilt Chamberlain">
-            <option value="Magic Johnson">
-            <option value="Kobe Bryant">
-            <option value="Stephen Curry">
-            <option value="Kevin Durant">
-            <option value="James Harden">
-            <option value="Shaquille O'Neal">
-            <option value="Tony Parker">
+                <option value="Lebron James">
+                <option value="Michael Jordan">
+                <option value="Wilt Chamberlain">
+                <option value="Magic Johnson">
+                <option value="Kobe Bryant">
+                <option value="Stephen Curry">
+                <option value="Kevin Durant">
+                <option value="James Harden">
+                <option value="Shaquille O'Neal">
+                <option value="Tony Parker">
+            </datalist>
             <input class="btn btn-primary" type="submit" value="Rechercher" id="submit">
-        </datalist>
     </form>
