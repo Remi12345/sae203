c93695490344d7f8e95a4e79e70be8ef87fc0598
Remi12345 <remi.droit@etudiant.univ-reims.fr>
2022-03-04 14:30:56 +0100

    imooration

--- listing.php
+++ listing.php
@@ -1,16 +1,21 @@
 <?php
 require 'debut_html.php';
 require 'header.php';
-
+?>
+<html>
+    <div id="div">
+<?php
 $mabd = new PDO('mysql:host=localhost;dbname=sae203;charset=UTF8;', 'sae203', 'MotDePasseFortDeVotreChoix');
 $mabd->query('SET NAMES utf8;');
 $req = "SELECT * FROM meilleurs_joueurs_nba INNER JOIN franchises_nba ON meilleurs_joueurs_nba._franchises_id = franchises_nba.franchises_id";
 $resultat = $mabd->query($req);
 foreach ($resultat as $value) {
     echo $value['jnba_classement'] .'e'.' - ' . $value['jnba_nom'];
-    echo '<br>' .'<p class="classement1">'. $value['franchises_nom'];
+    echo '<br>' .'<p class="classements">'. $value['franchises_nom'];
     echo '<br>'.'<p class="classement">'. $value['jnba_poste'];
     echo '<br>' .'<p class="classement">'. $value['jnba_nationalite'];
-    echo '<br/>'.1'<img class="image" src="photos/'.$value['jnba_photo'].'">'.'<hr>';
+    echo '<br/>'.'<img class="image" src="photos/'.$value['jnba_photo'].'">'.'<hr>';
 }
 ?>
+</div>
+</html>
