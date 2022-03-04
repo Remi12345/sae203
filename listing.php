ce5e5c3f71d4891ffd53bcdc5c3a05df46655858
Remi12345 <remi.droit@etudiant.univ-reims.fr>
2022-03-04 15:52:40 +0100

    imooration

--- listing.php
+++ listing.php
@@ -10,11 +10,11 @@ $mabd->query('SET NAMES utf8;');
 $req = "SELECT * FROM meilleurs_joueurs_nba INNER JOIN franchises_nba ON meilleurs_joueurs_nba._franchises_id = franchises_nba.franchises_id";
 $resultat = $mabd->query($req);
 foreach ($resultat as $value) {
-    echo '<div class="divs">'.'<p class="classement">'.$value['jnba_classement'] .'e'.' - ' .'<p class="classements">'. $value['jnba_nom'].'</div>';
-    echo '<div class="divs">'.'<p class="classement">'. $value['franchises_nom'].'</div>';
-    echo '<div class="divs">'.'<p class="classement">'. $value['jnba_poste'].'</div>';
-    echo '<div class="divs">'.'<p class="classement">'. $value['jnba_nationalite'].'</div>';
-    echo '<div class="divs">'.'<br/>'.'<img class="image" src="photos/'.$value['jnba_photo'].'">'.'</div>'.'<hr>';
+    echo '<article class="divs">'.'<p class="classement">'.$value['jnba_classement'] .'e'.' - ' .'<p class="classements">'. $value['jnba_nom'].'</article>';
+    echo '<article class="divs">'.'<p class="classement">'. $value['franchises_nom'].'</article>';
+    echo '<article class="divs">'.'<p class="classement">'. $value['jnba_poste'].'</article>';
+    echo '<article class="divs">'.'<p class="classement">'. $value['jnba_nationalite'].'</article>';
+    echo '<article class="divs">'.'<br/>'.'<img class="image" src="photos/'.$value['jnba_photo'].'">'.'</article>'.'<hr>';
 }
 ?>
 </div>
