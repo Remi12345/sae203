6ca35ac9fa951078d914a15b656c34222748a568
Remi12345 <remi.droit@etudiant.univ-reims.fr>
2022-03-04 14:51:00 +0100

    imooration

--- listing.php
+++ listing.php
@@ -10,7 +10,7 @@ $mabd->query('SET NAMES utf8;');
 $req = "SELECT * FROM meilleurs_joueurs_nba INNER JOIN franchises_nba ON meilleurs_joueurs_nba._franchises_id = franchises_nba.franchises_id";
 $resultat = $mabd->query($req);
 foreach ($resultat as $value) {
-    echo '<div class="divs">'.$value['jnba_classement'] .'e'.' - ' . $value['jnba_nom'].'</div>';
+    echo '<div class="divs">'.$value['jnba_classement'] .'e'.' - ' .'<p class="classements">'. $value['jnba_nom'].'</div>';
     echo '<div class="divs">'.'<br>' .'<p class="classements">'. $value['franchises_nom'].'</div>';
     echo '<div class="divs">'.'<br>'.'<p class="classement">'. $value['jnba_poste'].'</div>';
     echo '<div class="divs">'.'<br>' .'<p class="classement">'. $value['jnba_nationalite'].'</div>';
