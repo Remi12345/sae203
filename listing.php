343d7b660966f7488ea6a4dfeeb1640b8e2cea90
Remi12345 <remi.droit@etudiant.univ-reims.fr>
2022-03-04 14:27:31 +0100

    imooration

--- listing.php
+++ listing.php
@@ -8,9 +8,9 @@ $req = "SELECT * FROM meilleurs_joueurs_nba INNER JOIN franchises_nba ON meilleu
 $resultat = $mabd->query($req);
 foreach ($resultat as $value) {
     echo $value['jnba_classement'] .'e'.' - ' . $value['jnba_nom'];
-    echo '<br>' .'<p class="classement">'. $value['franchises_nom'];
+    echo '<br>' .'<p class="classement1">'. $value['franchises_nom'];
     echo '<br>'.'<p class="classement">'. $value['jnba_poste'];
     echo '<br>' .'<p class="classement">'. $value['jnba_nationalite'];
-    echo '<br/>'.'<img class="image" src="photos/'.$value['jnba_photo'].'">'.'<hr>';
+    echo '<br/>'.1'<img class="image" src="photos/'.$value['jnba_photo'].'">'.'<hr>';
 }
 ?>
