73ef95cead3e895bb33633f1106b32c36b12acbe
Remi12345 <remi.droit@etudiant.univ-reims.fr>
2022-03-04 11:23:55 +0100

    test

---
+++ form1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>Connexion</h1>
+        <form method="post" action="t1.php">
+            <label for="email">e-mail : </label>
+            <input type="text" name="email" id="email"><br />
+            <label for="mdp">mot de passe : </label>
+            <input type="password" name="mdp" id="mdp"><br />
+            <input type="submit" value="Vérifier">
+        </form>
+    </body>
+</html>
+<?php
+    if ( (empty($_POST['email'])) || (empty($_POST['mdp'])) ){
+        header('Location: form1.php');
+    }
+
+    echo '<p>'.$_POST['email'].'</p>'."\n";
+    echo '<p>'.$_POST['mdp'].'</p>'."\n";
