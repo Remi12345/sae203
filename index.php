3a2e1b88b2acff5b1bb86798271724cb0c199471
Remi12345 <remi.droit@etudiant.univ-reims.fr>
2022-03-04 10:26:13 +0100

    test

--- index.php
+++ index.php
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-<?php
-require 'debut_html.php'
-require 'header.php'
-?>
-<link rel="stylesheet" href="style.css">
 <header>
-=======
 <!DOCTYPE html>
 <html lang="fr">
   <head>
     <meta charset="UTF-8">
     <title>Accueil</title>
     <meta name="description" content="">
-    <link rel="stylesheet" href="style.css">
   </head>
   <header>
->>>>>>> c098390b1dfaa37757c30ddec903ac5d4d094b0f
       <nav>
       <a href="#">Accueil</a>
       <a href="listing.php">Classement</a>
